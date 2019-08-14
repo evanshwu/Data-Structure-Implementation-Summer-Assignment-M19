9cdefb667e1fe22f7251510437f42af507c7ef7e
evanshwu <37651155+evanshwu@users.noreply.github.com>
2019-08-14 12:06:54 -0700

    First commit

---
+++ src/MystSafeguard.java
@@ -0,0 +1,164 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+
+public class MystSafeguard extends Thread{
+	int indexMin = 0;
+	int indexMax = 0;
+	int indexOfChoices = 0;
+	int lastNumOfRange = 0;
+	
+	ArrayList<timetable> timetables = new ArrayList<timetable>();
+//	ArrayList<Integer> regList= new ArrayList<Integer>();// 0=empty, 1=fill
+	int result = indexMax-indexMin;
+	
+	public String getFireGuard(){
+		lastNumOfRange = indexMax-indexMin;
+		result = lastNumOfRange;
+		for(int k=0;k<indexOfChoices;k++){		
+			int startTimeT = timetables.get(k).getStartTime();
+			int endTimeT = timetables.get(k).getEndTime();
+			
+			for(int k2=0;k2<indexOfChoices;k2++){
+				if(k!=k2){
+//					System.out.println("compare "+k+" and "+k2+" where N is "+startTimeT+","+endTimeT);
+					int startTimeN = timetables.get(k2).getStartTime();
+					int endTimeN = timetables.get(k2).getEndTime();
+//					System.out.println("...check with "+startTimeN+" and "+endTimeN);
+					if(startTimeT>startTimeN && startTimeT<endTimeN ){
+//						System.out.println("......in middle!");
+						for(int j = startTimeT;j<=endTimeT;j++){
+//							System.out.println("......trying at "+j+", compare with N which is "+startTimeN+","+endTimeN);
+							if(j>startTimeT && j<=endTimeN){
+//								System.out.println("......!!!SWAPED startT to "+j);
+								startTimeT = j;
+							}
+						}
+					}else if(endTimeT>startTimeN && endTimeT<endTimeN){
+//						System.out.println("......in middle!");
+						for(int j = endTimeT;j>=startTimeT;j--){
+//							System.out.println("......trying at "+j+", compare with N which is "+startTimeN+","+endTimeN);
+							if(j>=startTimeN){
+//								System.out.println("......!!!SWAPED endT to "+j);
+								endTimeT = j;
+							}
+						}
+					}
+					
+				}
+			}
+//			System.out.println("[RESULT]Now comparing "+(endTimeT-startTimeT)+" with "+result);
+			if((endTimeT-startTimeT)<result){
+				result = endTimeT-startTimeT;
+			}
+//			this.clearList();
+			System.out.println("##finish "+k+" out of "+indexOfChoices);
+		}
+		timetables.clear();
+		
+		return String.valueOf(lastNumOfRange-result);
+	}
+	
+//	public void clearList(){
+//		for(int i=0;i<regList.size();i++){
+//			regList.set(i, 0);
+//		}
+//	}
+//	
+//	public int getBlanks(){
+//		int count = 0;
+//		
+//		for(int _res : regList){
+//			if(_res==0){
+//				count++;
+//			}
+//		}
+//		
+//		return regList.size()-count;
+//	}
+	
+	public void getMaxIndex(){
+		for(timetable t:timetables){
+			if(t.getEndTime()>indexMax){
+				indexMax = t.getEndTime();
+			}
+		}
+	}
+	
+	public void getMinIndex(){
+		indexMin = indexMax;
+		for(timetable t:timetables){
+			if(t.getStartTime()<indexMin){
+				indexMin = t.getStartTime();
+			}
+		}
+	}
+	
+//	public void initList(){
+//		for(int num = indexMin;num<indexMax;num++){
+//			regList.add(0);
+//		}
+//	}
+	
+	public void readFromFile(String fileName){
+		// setup indexOfChoices
+		
+		BufferedReader reader;
+		try{
+			reader = new BufferedReader(new FileReader(fileName));
+			String line = reader.readLine();
+			int readerIndex = 1;
+			while(line!=null){
+				if(readerIndex==1){
+//					System.out.println("indexOfChoices="+line);
+					this.indexOfChoices = Integer.valueOf(line.trim());
+				}else{
+					String[] times = line.split("\\s+");
+					timetable t = new timetable();
+					t.setStartTime(Integer.valueOf(times[0].trim()));
+					t.setEndTime(Integer.valueOf(times[1].trim()));
+					timetables.add(t);
+				}
+				line = reader.readLine();
+				readerIndex++;
+			}
+			reader.close();
+			this.getMaxIndex();
+			this.getMinIndex();
+//			this.initList();
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	public void writeToFile(String fileName, String content){
+		try{
+			BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+			writer.write(content);
+			writer.close();
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	public void execute(){
+		String[] inputs = {"inputs/1.in", "inputs/2.in", "inputs/3.in", "inputs/4.in", "inputs/5.in", "inputs/6.in", "inputs/7.in", "inputs/8.in", "inputs/9.in", "inputs/10.in"};
+		String[] outputs = {"outputs/1.out", "outputs/2.out", "outputs/3.out", "outputs/4.out", "outputs/5.out", "outputs/6.out", "outputs/7.out", "outputs/8.out", "outputs/9.out", "outputs/10.out"};
+		
+		for(int i=4;i<5;i++){
+			System.out.println("start task no."+i);
+			this.readFromFile(inputs[i]);
+			writeToFile(outputs[i], this.getFireGuard());
+			System.gc();
+		}
+		
+		
+	}
+	
+	public static void main(String[] args){
+		MystSafeguard ms = new MystSafeguard();
+		ms.execute();
+	}
+}
---
+++ src/timetable.java
@@ -0,0 +1,18 @@
+
+public class timetable {
+	public int startTime;
+	public int endTime;
+	
+	public int getStartTime() {
+		return startTime;
+	}
+	public void setStartTime(int startTime) {
+		this.startTime = startTime;
+	}
+	public int getEndTime() {
+		return endTime;
+	}
+	public void setEndTime(int endTime) {
+		this.endTime = endTime;
+	}
+}
